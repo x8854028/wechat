81398b249724c995c6a01c81b3a01acd15de9f36
x8854028 <41317483+x8854028@users.noreply.github.com>
2019-05-20 13:21:57 +0800

    movieRead

---
+++ pages/posts/posts.js
@@ -0,0 +1,76 @@
+// pages/posts/posts.js
+var postsData = require("../../data/posts-data.js")
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    date:'Nov 18 2020',
+    
+    title: "正是虾肥蟹壮时",
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+   
+    this.setData({
+      posts_key:postsData.postList
+    })
+     
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
